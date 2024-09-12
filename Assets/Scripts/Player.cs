4c95f2b946684a9e2014da6436bf7ee8c6cf0f80
Levente <szebik.levente@gmail.com>
2024-09-12 13:04:19 +0200

    Player kepernyon belul tartasa

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,18 +7,44 @@ public class Player : MonoBehaviour
 
     public float moveSpeed = 5f; // Player mozgási sebessége
 
-    // Update hívódik minden frame-ben
+    private Vector2 screenBounds; // A képernyõ szélei
+    private float objectWidth;
+    private float objectHeight;
+
+    void Start()
+    {
+        // A képernyõ széleinek kiszámítása
+        Camera cam = Camera.main;
+        screenBounds = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, cam.transform.position.z));
+
+        // A hajó méretének kiszámítása
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+    }
+
     void Update()
     {
-        // Input detektálása a vízszintes (Horizontal) és függõleges (Vertical) tengelyeken
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
 
-        // Mozgás vektor kiszámítása
         Vector3 move = new Vector3(moveX, moveY, 0);
-
-        // Mozgás alkalmazása
         transform.position += move * moveSpeed * Time.deltaTime;
+
+        // A játékos képernyõn belül tartása
+        keepPlayerInBounds();
+    }
+
+    void keepPlayerInBounds()
+    {
+        // Játékos aktuális pozíciója
+        Vector3 pos = transform.position;
+
+        // Pozíció korlátozása a képernyõn belülre
+        pos.x = Mathf.Clamp(pos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        pos.y = Mathf.Clamp(pos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+
+        // A frissített pozíció beállítása
+        transform.position = pos;
     }
 
 }
