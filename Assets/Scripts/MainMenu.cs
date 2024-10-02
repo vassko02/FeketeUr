deaa1bd0ced7526e9dcf5d53ecfff3b1a59adf0f
Levente <szebik.levente@gmail.com>
2024-10-02 14:48:38 +0200

    Menu zene és hozzá a slider, mentés kész

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,27 +2,41 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private AudioMixer Mixer;
+    private const string VolumePrefKey = "AudioVolume";
     public void PlayGame()
     {
-        Debug.Log("klikk");
         UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Level1");
     }
     public void Quit()
     {
-        Debug.Log("asd");    
         Application.Quit();
     }
+
+    public void Options()
+    {
+        SceneManager.LoadSceneAsync("Options");
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float savedVolume = PlayerPrefs.GetFloat(VolumePrefKey, 1f); // Default to 1 (max volume) if not set
+        SetVolume(savedVolume);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SetVolume(float value)
+    {
+        Mixer.SetFloat("Volume", Mathf.Log10(value) * 20);
     }
 }
